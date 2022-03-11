a1f509c11b4fac0457264deb3253dc13587841d4
Neil <88711171+21Neil@users.noreply.github.com>
2022-03-11 16:34:09 +0800

    Finish calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  // let sum = 0;
+  // arr.map(num => (sum += num));
+  // return sum;
+  return arr.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((accumulator, currentValue) => accumulator *= currentValue)
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  // let pow = 1;
+  // for(let i = 0; i < b; i++){
+  //   pow *= a
+  // }
+  // return pow;
+  return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  // let factorial = 1; 
+  // for(let i = 1; i <= num; i++){
+  //   factorial *= i
+  // }
+  // return factorial;
+  if(num === 0){
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
