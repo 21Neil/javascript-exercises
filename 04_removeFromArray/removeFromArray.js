de3b00be573b71afc1b08f67e384fe5c709826ee
Neil <88711171+21Neil@users.noreply.github.com>
2022-03-11 15:55:41 +0800

    Finish 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,30 @@
-const removeFromArray = function() {
+// const removeFromArray = function(...argus) {
+//   const array = argus[0];
+//   return array.filter( val => !argus.includes(val))
+// }
 
+// const removeFromArray = function(...argus) {
+//   const arr = argus[0];
+//   const newArr = [];
+//   arr.forEach( val => {
+//     if(!argus.includes(val)){
+//       newArr.push(val)
+//     }
+//   })
+//   return newArr;
+// }
+
+const removeFromArray = function(arr, ...nums) {
+  let result = arr.filter((item) => {
+    let count = 0;
+    for (const num of nums) {
+      if (item === num) {
+        count += 1;
+      }
+    }
+    return count === 0
+  });
+  return result;
 };
 
 // Do not edit below this line
